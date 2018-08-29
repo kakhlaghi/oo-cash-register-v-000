5eae342fd2f5e2e3fed24e91838fda62a04bd3fb
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-08-29 22:34:39 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,6 +20,7 @@ class CashRegister
       self.total = self.total * (1-(self.discount/100.0))
       return "After the discount, the total comes to $#{self.total.to_i}."
     end
+    binding.pry
   end
   
   def items
